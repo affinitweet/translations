8affea98918530193e4aec9b03abfc685c3319fe
Thomas Reichling <HapLifeMan@users.noreply.github.com>
2019-08-12 13:14:33 +0200

    Added translators file

---
+++ translators.js
@@ -0,0 +1,30 @@
+export default {
+  fr: {
+    author: 'Thomas Reichling',
+    language: 'french',
+  },
+  en: {
+    author: 'Thomas Reichling',
+    language: 'english',
+  },
+  es: {
+    author: 'Valerie Blanco',
+    language: 'spanish',
+  },
+  pl: {
+    author: 'Patryk Gawroński',
+    language: 'polish',
+  },
+  'pt-br': {
+    author: 'Allan Fernando',
+    language: 'portuguese',
+  },
+  'zh-cn': {
+    author: 'Kagurazaka Yashi',
+    language: 'simplifiedChinese',
+  },
+  'zh-tw': {
+    author: 'Kagurazaka Yashi',
+    language: 'traditionalChinese',
+  },
+}
