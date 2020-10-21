c30d5c67731a0129729b17e5395e525eda17d2c8
dionysii <73240662+dionysii@users.noreply.github.com>
2020-10-21 18:30:26 +0300

    Update back.js

--- uk/back.js
+++ uk/back.js
@@ -1,1 +1,101 @@
+export default {
 
+  /* Steps */
+  _st: {
+    received: 'Request received by the server...',
+    starting: 'Starting...',
+    fetchingData: 'Fetching data...',
+    fetchingTweets: 'Fetching tweets...',
+    computingData: 'Computing data...',
+    generatingImage: 'Generating image...',
+    uploadingMedia: 'Uploading image...',
+    publishingTweet: 'Publishing tweet...',
+    savingResults: 'Saving results...',
+  },
+
+  /* Features */
+  _ft: {
+    'twitter-activity': {
+      basedOnLastDays: 'Based on the last {days} days...',
+      countingTweets: '... and counting {count} tweets:',
+      interactionsReceived: 'Interactions received',
+      last30Days: '*: last 30 days',
+      tweetingHours: 'Tweeting hours',
+      tweetRepartition: 'Tweet distribution',
+    },
+    'first-follower': {
+      yourFirstFollowerIs: 'Your first follower is',
+      dateFormat: 'on {month}/{day}/{year} at {hour}:{minute}:{second}',
+    },
+    'first-following': {
+      yourFirstFollowingIs: 'Your first following is',
+      dateFormat: 'on {month}/{day}/{year} at {hour}:{minute}:{second}',
+    },
+    'first-to-follow': {
+      youAreNotFollowing: "You're not following {user}...",
+      notFollowingYou: '{user} is not following you...',
+      youFollowed: 'You followed {user}: ',
+      followedYou: '{user} followed you: ',
+      dateFormat: 'on {month}/{day}/{year} at {hour}:{minute}:{second}',
+      dateFormat2: 'on {month}/{day}/{year} at {hour}h',
+    },
+    friendships: {
+      average: 'Friendships average of',
+      following: 'You\'re following them',
+      'not-following': "You're not following them",
+      followed_by: 'They\'re following you',
+      'not-followed_by': "They're not following you",
+      notifications_enabled: 'You are notified when they tweets',
+      'not-notifications_enabled': "You're not notified when they tweets",
+      blocking: 'You blocked them',
+      'not-blocking': "You didn't block them",
+      blocked_by: 'They blocked you',
+      'not-blocked_by': "They didn't block you",
+      muting: 'You mute them',
+      'not-muting': "You didn't mute them",
+      want_retweets: 'You see their retweets',
+      'not-want_retweets': "You don't see their retweets",
+      marked_spam: 'You marked them as spam',
+      'not-marked_spam': "You didn't mark them as spam",
+    },
+    'tweet-rate': {
+      average: '{user} tweets on average...',
+      tweetsPerDay: '{count} times a day',
+    },
+    'common-friends': {
+      friendsRemaining: '... and {count} other friends !',
+      noFriends: 'No common friends...',
+      commonFriends: 'I have {count} common friends with {user} !',
+      noCommonFriends: 'I have no common friends with {user}...',
+    },
+    valentine: 'Your Valentine {year} is',
+    'stolen-kiss': 'Your Stolen Kiss is',
+    'hidden-twin': 'Your Hidden Twin is',
+    'secret-crush': 'Your Secret Crush is',
+    'may-day': 'I give a Lily-of-the-Valley 💐 to',
+    'halloween-duo': 'Your Halloween Duo is',
+    'new-years-kiss': "My New Year's Kiss is",
+  },
+
+  /* Tweet content */
+  _tw: {
+    'first-to-follow': '{text}',
+    friendships: '{text}',
+    'top-friends': '{users}',
+    'famous-followers': '{users}',
+    'common-friends': '{text}',
+    'tweet-rate': '{text}',
+    'emoji-mood': '{emojis}',
+    valentine: "This year you're mine {user}! ❤️\nAnd you, whom will be yours?",
+    'twitter-activity': "Here's my account statistics from the past {days} days. Do you have better interactions?",
+    'first-follower': 'I just discovered my first follower on Twitter, it\'s {user} and they followed me {date}!\nWho\'s your first follower?',
+    'first-following': 'I just discovered my first following on Twitter, it\'s {user} and I followed them {date}!\nWho\'s your first following?',
+    'secret-crush': '{user} secretly loves you! 😳\nWho loves you in secret?',
+    'hidden-twin': '{user} is my hidden twin 😱\nFind out who is yours!',
+    'stolen-kiss': '{user} would kiss you around the corner... 😏\nAnd you, who would do it?',
+    'may-day': '{user}, I give you my best bouquet of lily-of-the-valley! 💐\nTo whom will you give yours?',
+    'halloween-duo': "I don't even have to dress up this year to be scary, I'm with {user}! 🎃👻\nAnd you, with whom will you scare the most?",
+    'new-years-kiss': "I'm preparing my best last year kiss and first of the next year {user} 😘💕\nAnd you, who will you kiss at midnight?",
+  },
+
+}
